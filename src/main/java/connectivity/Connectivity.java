cce2593bd1e601850cceba7c06bcc494e8c8cc6b
hanifaudah <hanifarkanaudah@gmail.com>
2020-05-12 07:04:11 +0700

    foo

---
+++ src/main/java/connectivity/Connectivity.java
@@ -0,0 +1,22 @@
+package connectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connectivity {
+    private Connection connection;
+    public Connection getConnection() {
+        String dbName = "expenses_tracker_db";
+        String dbUserName = "root";
+        String dbPassWord = System.getenv("MYSQL_DB_PASSWORD");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/"+dbName,dbUserName,dbPassWord);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
